77f22cc3c27c6906c35eb82be77c351880f09a53
Hexa <hexa.diary@gmail.com>
2011-06-18 02:44:27 +0900

    ソースの取得先を変更した

--- ruby/attributes/default.rb
+++ ruby/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:ruby][:install_method] = "source"
-default[:ruby][:url] = "ftp://ftp.ruby-lang.org/pub/ruby/1.8"
+default[:ruby][:url] = "http://core.ring.gr.jp/archives/lang/ruby/1.8"
 default[:ruby][:version] = "1.8.7"
 default[:ruby][:patch_level] = "p334"
 default[:ruby][:checksum] = "68f68d6480955045661fab3be614c504bfcac167d070c6fdbfc9dbe2c5444bc0"
